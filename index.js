871a0c10387124d36615fcc4c2360c4eed979bf5
José Ferrari <licoverpa321@gmail.com>
2024-06-18 21:00:47 -0300

    adding decorator

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import TXTDecorator from "./src/TXTDecorator.js";
 const [cmd, script, param1] = process.argv,
   filename = "./data/cidades-2.json";
 
-const reporter = new CitiesReporter(
-  { formatType: "html" },
-  (city) => new TXTDecorator(city)
+const reporter = new CitiesReporter({ formatType: param1 }, (city) =>
+  param1 === "html" ? new HTMLDecorator(city) : new TXTDecorator(city)
 );
 reporter.report("./data/cidades-2.json").then(console.log);
 
 // Added Factory creational pattern
 // Added Strategy behavioral pattern
+// Added Decorator structural pattern
